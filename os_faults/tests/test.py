8aeb579dcc61369df7853e00e747bf0263e5448d
Yaroslav Lobankov <ylobankov@mirantis.com>
2016-09-08 19:24:02 +0300

    Adding unit tests for libvirt driver
    Change-Id: I16ce625fb0c66fc74534b10ec89831eb9516ea16

--- os_faults/tests/test.py
+++ os_faults/tests/test.py
@@ -1,6 +1,3 @@
-# Copyright 2010-2011 OpenStack Foundation
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
-#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
@@ -17,5 +14,4 @@ from oslotest import base
 
 
 class TestCase(base.BaseTestCase):
-
     """Test case base class for all unit tests."""
