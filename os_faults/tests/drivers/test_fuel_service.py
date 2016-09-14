2e5c391e3735e83fa9ce618da99dae9775e3cf60
Alexey Zaytsev <azaytsev@mirantis.com>
2016-09-14 13:59:10 +0300

    Unit tests for FuelService
    Added unit tests for follow methods of FuelService class:
    - kill
    - freeze/unfreeze
    - unplug/plug
    
    Change-Id: Iadc96e885ace48ee3446d75b6e055263c84307cd

---
+++ os_faults/tests/drivers/test_fuel_service.py
@@ -0,0 +1,217 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ddt
+import mock
+
+from os_faults.drivers import fuel
+from os_faults.tests import fake
+from os_faults.tests import test
+
+
+@ddt.ddt
+class FuelServiceTestCase(test.TestCase):
+
+    @mock.patch('os_faults.ansible.executor.AnsibleRunner', autospec=True)
+    @ddt.data(('keystone', fuel.KeystoneService),
+              ('mysql', fuel.MySQLService),
+              ('rabbitmq', fuel.RabbitMQService),
+              ('nova-api', fuel.NovaAPIService),
+              ('glance-api', fuel.GlanceAPIService))
+    @ddt.unpack
+    def test_kill(self, service_name, service_cls, mock_ansible_runner):
+        ansible_runner_inst = mock_ansible_runner.return_value
+        ansible_runner_inst.execute.side_effect = [
+            [fake.FakeAsnibleResult(payload={
+                'stdout': '[{"ip": "10.0.0.2"}, {"ip": "10.0.0.3"}]',
+            })],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')]
+        ]
+
+        fuel_managment = fuel.FuelManagement({
+            'address': 'fuel.local',
+            'username': 'root',
+        })
+
+        service = fuel_managment.get_service(service_name)
+        self.assertIsInstance(service, service_cls)
+
+        service.kill()
+        ansible_runner_inst.execute.assert_has_calls([
+            mock.call(['fuel.local'], {'command': 'fuel2 node list -f json'}),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command': service_cls.GET_NODES_CMD}, []),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command': service_cls.KILL_CMD}),
+        ])
+
+    @mock.patch('os_faults.ansible.executor.AnsibleRunner', autospec=True)
+    @ddt.data(('keystone', fuel.KeystoneService),
+              ('mysql', fuel.MySQLService),
+              ('rabbitmq', fuel.RabbitMQService),
+              ('nova-api', fuel.NovaAPIService),
+              ('glance-api', fuel.GlanceAPIService))
+    @ddt.unpack
+    def test_freeze(self, service_name, service_cls, mock_ansible_runner):
+        ansible_runner_inst = mock_ansible_runner.return_value
+        ansible_runner_inst.execute.side_effect = [
+            [fake.FakeAsnibleResult(payload={
+                'stdout': '[{"ip": "10.0.0.2"}, {"ip": "10.0.0.3"}]',
+            })],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')]
+        ]
+
+        fuel_managment = fuel.FuelManagement({
+            'address': 'fuel.local',
+            'username': 'root',
+        })
+
+        service = fuel_managment.get_service(service_name)
+        self.assertIsInstance(service, service_cls)
+
+        service.freeze()
+        ansible_runner_inst.execute.assert_has_calls([
+            mock.call(['fuel.local'], {'command': 'fuel2 node list -f json'}),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command': service_cls.GET_NODES_CMD}, []),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command': service_cls.FREEZE_CMD}),
+        ])
+
+    @mock.patch('os_faults.ansible.executor.AnsibleRunner', autospec=True)
+    @ddt.data(('keystone', fuel.KeystoneService),
+              ('mysql', fuel.MySQLService),
+              ('rabbitmq', fuel.RabbitMQService),
+              ('nova-api', fuel.NovaAPIService),
+              ('glance-api', fuel.GlanceAPIService))
+    @ddt.unpack
+    def test_unfreeze(self, service_name, service_cls, mock_ansible_runner):
+        ansible_runner_inst = mock_ansible_runner.return_value
+        ansible_runner_inst.execute.side_effect = [
+            [fake.FakeAsnibleResult(payload={
+                'stdout': '[{"ip": "10.0.0.2"}, {"ip": "10.0.0.3"}]',
+            })],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')]
+        ]
+
+        fuel_managment = fuel.FuelManagement({
+            'address': 'fuel.local',
+            'username': 'root',
+        })
+
+        service = fuel_managment.get_service(service_name)
+        self.assertIsInstance(service, service_cls)
+
+        service.unfreeze()
+        ansible_runner_inst.execute.assert_has_calls([
+            mock.call(['fuel.local'], {'command': 'fuel2 node list -f json'}),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command': service_cls.GET_NODES_CMD}, []),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command': service_cls.UNFREEZE_CMD}),
+        ])
+
+    @mock.patch('os_faults.ansible.executor.AnsibleRunner', autospec=True)
+    @ddt.data(('mysql', fuel.MySQLService))
+    @ddt.unpack
+    def test_unplug(self, service_name, service_cls, mock_ansible_runner):
+        ansible_runner_inst = mock_ansible_runner.return_value
+        ansible_runner_inst.execute.side_effect = [
+            [fake.FakeAsnibleResult(payload={
+                'stdout': '[{"ip": "10.0.0.2"}, {"ip": "10.0.0.3"}]',
+            })],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')]
+        ]
+
+        fuel_managment = fuel.FuelManagement({
+            'address': 'fuel.local',
+            'username': 'root',
+        })
+
+        service = fuel_managment.get_service(service_name)
+        self.assertIsInstance(service, service_cls)
+
+        service.unplug()
+        ansible_runner_inst.execute.assert_has_calls([
+            mock.call(['fuel.local'], {'command': 'fuel2 node list -f json'}),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command': service_cls.GET_NODES_CMD}, []),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command':
+                      service_cls.UNPLUG_CMD.format(service_cls.PORT)}),
+        ])
+
+    @mock.patch('os_faults.ansible.executor.AnsibleRunner', autospec=True)
+    @ddt.data(('mysql', fuel.MySQLService))
+    @ddt.unpack
+    def test_plug(self, service_name, service_cls, mock_ansible_runner):
+        ansible_runner_inst = mock_ansible_runner.return_value
+        ansible_runner_inst.execute.side_effect = [
+            [fake.FakeAsnibleResult(payload={
+                'stdout': '[{"ip": "10.0.0.2"}, {"ip": "10.0.0.3"}]',
+            })],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')],
+            [fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.2'),
+             fake.FakeAsnibleResult(payload={'stdout': ''},
+                                    host='10.0.0.3')]
+        ]
+
+        fuel_managment = fuel.FuelManagement({
+            'address': 'fuel.local',
+            'username': 'root',
+        })
+
+        service = fuel_managment.get_service(service_name)
+        self.assertIsInstance(service, service_cls)
+
+        service.plug()
+        ansible_runner_inst.execute.assert_has_calls([
+            mock.call(['fuel.local'], {'command': 'fuel2 node list -f json'}),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command': service_cls.GET_NODES_CMD}, []),
+            mock.call(['10.0.0.2', '10.0.0.3'],
+                      {'command':
+                      service_cls.PLUG_CMD.format(service_cls.PORT)}),
+        ])
